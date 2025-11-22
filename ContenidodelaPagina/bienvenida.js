9a978c6b83bd7b223ca75c4513dacff94e037cc5
Frida Sofia <frida.rodriguezg@uanl.edu.mx>
2025-11-22 17:40:54 -0600

    contenido limitado de la pagina

---
+++ ContenidodelaPagina/bienvenida.js
@@ -0,0 +1,7 @@
+/* bienvenida.js */
+$(function(){
+  // Redirección simulada
+  $('#btn-login, #btn-register, .ver-receta').on('click', function(){
+    alert('Debes iniciar sesión para acceder.');
+  });
+});
